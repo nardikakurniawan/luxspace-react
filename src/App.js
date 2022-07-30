fb709da8ea009ac14e2c46007ab8594f156ce1ce
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-07-30 15:13:20 +0700

    Section HomePage & Hero

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "assets/css/app.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import HomePage from "pages/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-700 font-bold">Hello World</h1>
+      <Router>
+        <Route path="/" component={HomePage} />
+      </Router>
     </div>
   );
 }
