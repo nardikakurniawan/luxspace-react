cedee7bc26658d903c93d5c02a7b53c718580284
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-08-13 11:32:34 +0700

    useScrollAnchor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,14 +1,18 @@
 import React from "react";
 
 import Header from "parts/Header";
-import Hero from "parts/Hero";
+import Hero from "parts/HomePage/Hero";
 import Clients from "parts/Clients";
 import SiteMap from "parts/SiteMap";
 import Footer from "parts/Footer";
 import BrowseRoom from "parts/HomePage/BrowseRoom";
 import JustArrived from "parts/HomePage/JustArrived";
 
+import useScrollAnchor from "helpers/hooks/useScrollAnchor";
+
 export default function HomePage() {
+  useScrollAnchor();
+
   return (
     <>
       <Header theme="white" position="absolute" />
