c41431af5530b68e8fa6ee64e056b3f07b8ba308
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-08-15 22:03:52 +0700

    JustArrived => skeleton

--- src/parts/HomePage/JustArrived.js
+++ src/parts/HomePage/JustArrived.js
@@ -7,6 +7,23 @@ import Carousel from "components/Carousel";
 import useAsync from "helpers/hooks/useAsync";
 import fetch from "helpers/fetch";
 
+function Loading() {
+  return Array(6)
+    .fill()
+    .map((_, index) => {
+      return (
+        <div className="px-4 relative card group" key={index}>
+          <div
+            className="rounded-xl bg-gray-300 overflow-hidden card-shadow relative"
+            style={{ width: 287, height: 386 }}
+          ></div>
+          <div className="w-24 h-3 bg-gray-300 mt-3 rounded-full"></div>
+          <div className="w-36 h-3 bg-gray-300 mt-2 rounded-full"></div>
+        </div>
+      );
+    });
+}
+
 export default function JustArrived() {
   const { data, status, error, run, isLoading } = useAsync();
 
@@ -33,7 +50,15 @@ export default function JustArrived() {
         {/* <!-- <div className="overflow-hidden z-10"> --> */}
 
         {isLoading ? (
-          <div className="flex -mx-4 flex-row relative">Loading</div>
+          <div
+            className="flex -mx-4 flex-row relative"
+            style={{
+              paddingLeft:
+                refContainer.current?.getBoundingClientRect?.()?.left - 16 || 0,
+            }}
+          >
+            <Loading />
+          </div>
         ) : error ? (
           JSON.stringify(error)
         ) : data.data.length === 0 ? (
