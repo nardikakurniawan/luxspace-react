d541b5f93234d1bf897ff55b14697e35104d9136
Nardika Kurniawan <nardikakurniawan20@gmail.com>
2022-08-15 17:47:37 +0700

    Just Arrived

--- src/parts/HomePage/BrowseRoom.js
+++ src/parts/HomePage/BrowseRoom.js
@@ -59,9 +59,7 @@ function Loading({ ratio = {} }) {
 }
 
 export default function BrowseRoom() {
-  const { data, status, error, run, isLoading } = useAsync({
-    data: { username: "" },
-  });
+  const { data, status, error, run, isLoading } = useAsync();
 
   useEffect(() => {
     run(fetch({ url: "/api/categories/?page=1&limit=4" }));
